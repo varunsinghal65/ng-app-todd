5aecbb518b65bcbb1d0b539bfe2a7abfe42c5551
vsinghal <varun.singhal@amadeus.com>
2020-02-11 21:40:37 +0530

    1st component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
 
+/**
+ * Component is a "Decorator"
+ * Decorator is a function that annotates another function
+ * Using the component decorator against a typescript class tells angular about
+ * meta-data (like css, html), that will be used by this html element.
+ * 
+ * All components will create a html element
+ */
+import {Component} from '@angular/core';
+
+/**
+ * Component is a function, but since it is a decorator (TS feature), we just need to use "@Component".
+ * It will register this component with angular, telling it that this is a angular component.
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector : 'app-root',
+  styleUrls : ['app.component.scss'],
+  template : `
+    <div class='app'>
+    {{ title }}
+    </div>
+  `
 })
+
 export class AppComponent {
-  title = 'ng-app-todd';
+  //Nice way : All declarations here, init in constructor
+  title: string;
+  constructor () {
+    this.title = 'Ultimate Angular';
+  }
 }
