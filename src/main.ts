48b2da2960b95bb5855674bdaa6714957d924ceb
vsinghal <varun.singhal@amadeus.com>
2020-02-14 20:08:48 +0530

    Getting started : bootstrapping an angular application

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+/**
+ * This calss is used to tell angular to bootstrap our root module
+ * So flow, is 
+ * 
+ * -<app-root> is defined in index.html.
+ * -create a base component with 'app-root' as selector
+ * -bootstrap that components in a module (called root module)
+ * -finally bootstrap the module in angular
+ * 
+ * Now we want to compile angular in browser (also called JIT - just in time compiler), for which we will use 
+ * platformBrowserDynamic module. and not platformBrowser, it does not support DI and carries compilation in server
+ * (AOT - ahead of time compilation)
+ * 
+ */
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+ import {AppModule} from './app/app.module';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+ /**
+  * Bootstrapping an angular application
+  */
+ platformBrowserDynamic().bootstrapModule(AppModule);
