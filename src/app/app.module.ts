be66afb4588b84666891ab4351d86040426ba601
vsinghal <varun.singhal@amadeus.com>
2020-02-14 19:52:09 +0530

    Getting started : root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+/**
+ * This module was imported because we are creating a client application
+ */
+import { BrowserModule } from '@angular/platform-browser';
+/**
+ * This module is used because we want to use directives etcin this module.
+ */
+import { CommonModule } from  '@angular/common';
 
+/**
+ * @NgModule({X}), X provides module definition for "App Module"
+ * "bootstrap" : <componentclass> - used to indicate angular that this is a root/base module
+ *                                  .Tells angular that <componentclass> is to be boostrapped 
+ *                                   to index.html
+ * "declarations" : [array] - we need to register all the components that we wish to use in 
+ *                            this module
+ * "imports": [array] - sepcifies the list of modules that the components of RootModule would want to use
+ */
 @NgModule({
-  declarations: [
+  declarations : [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports : [
+    BrowserModule, CommonModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap : [AppComponent],
 })
-export class AppModule { }
+export class AppModule{}
