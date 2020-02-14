df2664fd154b7bdf24f83bd744b44f83eea909aa
vsinghal <varun.singhal@amadeus.com>
2020-02-14 20:23:58 +0530

    Templates : interpolation + expressions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,27 @@
 
-/**
- * Component is a "Decorator"
- * Decorator is a function that annotates another function
- * Using the component decorator against a typescript class tells angular about
- * meta-data (like css, html), that will be used by this html element.
- * 
- * All components will create a html element
- */
 import {Component} from '@angular/core';
 
-/**
- * Component is a function, but since it is a decorator (TS feature), we just need to use "@Component".
- * It will register this component with angular, telling it that this is a angular component.
- */
 @Component({
   selector : 'app-root',
   styleUrls : ['app.component.scss'],
   template : `
     <div class='app'>
-    {{ title }}
+      <!--Interpolation refers to embedding expressions into marked up text,
+       interpolation uses curly braces as delimiter -->
+      <!-- X in {{X}} is called expression --> 
+      <div>{{ title + '!'}}</div>
+      <div>{{numberOne}}</div>
+      <div>{{numberTwo + numberOne}}</div>
+      <div>{{ isHappy ? ':-)' : ':-(' }}
     </div>
   `
 })
 
 export class AppComponent {
   //Nice way : All declarations here, init in constructor
+  numberOne : number = 1;
+  numberTwo : number = 2;
+  isHappy : boolean = false;
   title: string;
   constructor () {
     this.title = 'Ultimate Angular';
