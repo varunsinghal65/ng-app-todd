a2498f7b04da656df599db3114e2bb8a1dc77b86
vsinghal <varun.singhal@amadeus.com>
2020-04-01 20:56:26 +0530

    Component architecture and feature modules : creating presentational components

---
+++ src/app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+/**
+ * Dumb or presentational or stateless child of container component
+ *
+ */
+@Component({
+selector : 'passenger-count',
+template : `
+    <div>
+    passenger count
+    </div>
+`
+})
+export class PassengerCountComponent {
+
+}
