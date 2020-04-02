b96e14d342a7f2b967139a20ec11e9b628d82ead
vsinghal <varun.singhal@amadeus.com>
2020-04-02 12:25:20 +0530

    Services, Http and Observables : Understanding @Injectable()

--- src/app/passenger-dashboard/services/passenger-dashboard.service.ts
+++ src/app/passenger-dashboard/services/passenger-dashboard.service.ts
@@ -1,7 +1,32 @@
 import { Passenger } from '../models/passenger.interface';
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
+/**
+ * What is @Injectable() ?
+ *
+ * In Angular, whenever you can do contructor based di, by simply decalring the deps in constructor.
+ * No Special decorator is needed.
+ *
+ * UNLESS
+ *
+ * the class where depenedencies are being injected is not decorated with decorator. Solution is that we decorate that
+ * class with a decorator, technically it can be any decorator, however its a good practice to use @Injectable()
+ *
+ * For e.g : "PassengerDashboardService" has a dependency on "HttpClient", Now when instantiating PassengerDashboardService,
+ * ng DI fails, because PassengerDashboardService does not have a decorator. So decorate it with  @Injectable().
+ *
+ * Note : This is the main reason why @Injectable() is not needed for constructor DI in a class decorated with @Component()
+ *
+ * More reading :
+ * https://blog.ninja-squad.com/2016/12/08/angular-injectable/
+ *
+ */
+@Injectable()
 export class PassengerDashboardService {
+     constructor(http : HttpClient) {
 
+     }
     getPassengers() : Passenger[] {
         return [{
             id: 1,
