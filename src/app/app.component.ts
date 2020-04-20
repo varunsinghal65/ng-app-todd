ba455e3b110394ef08c9ba213a42704c75c04375
varunsinghal65 <varunsinghal65@live.com>
2020-04-20 21:16:40 +0530

    Rendering Flows : ngClass and className bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 /**
- * ngFor
+ * ngClass and className PROPERT bindings
  * 
  */
 
@@ -16,22 +16,57 @@ interface Passenger {
   styleUrls: ['app.component.scss'],
   template: `
   <h3>Airline passengers</h3>
+    <!-- Let us say i want to display green and red dots against passengers names
+    based on their checking status -->
+    
+    <!-- Way 1 : Using class name--> 
+    
+    <!-- 
+    if pax.checkedIn evaluates to true, angular will 
+    add css class "checked-in" to the existing classes list
+     -->
   <ul>
-    <!-- "index" is exposed by Angular -->
     <li *ngFor="let pax of passengers; let i = index;">
-    {{ i }} : {{ pax.fullName }}
+      <span 
+       class="status"
+       [class.checked-in]="pax.checkedIn">
+      </span>
+      {{ i }} : {{ pax.fullName }}
     </li>
   </ul>
-      
-  <!-- Under the hood, angular actually converts the above sugar synatx into the following markup -->
-  <ul>
-    <ng-template ngFor let-pax let-i="index" [ngForOf]="passengers">  
-      <li>
-       {{ i }} : {{ pax.fullName }}
-      </li>
-    </ng-template>
+
+    <!-- Way 2 : Using ngClass--> 
+    
+    <!-- 
+    What if tomorrow, i have to add multiple CSS classes/styles based on the 
+    many component variables ?
+
+    Of course, we can add in SASS, and use way 1, but we can also use ngClass, which is much cleaner,
+    We suppy ngClass with the below configuration object : 
+    {
+      <CSS-CLASS-NAME> : <TS COMPONENT boolean variable>
+    }
+    if TS variable is true, "CSS-CLASS-NAME" is added to the list of CSS classes.
+     
+    In chrome dev tools, you can scan the LI elements and you will see the appropriate classes 
+    added 
+    -->
+    <ul>
+    <li *ngFor="let pax of passengers; let i = index;">
+      <span 
+       class="status"
+       [ngClass]="
+       {
+         'checked-in': pax.checkedIn, 
+         'checked-out': !pax.checkedIn
+       }
+       ">
+      </span>
+      {{ i }} : {{ pax.fullName }}
+    </li>
   </ul>
-    `
+
+`
 })
 
 export class AppComponent {
