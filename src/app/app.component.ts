342aa14e16bebc5600b2d31be5a4e450f5871c26
varunsinghal65 <varunsinghal65@live.com>
2020-04-20 22:25:20 +0530

    Rendering Flows : Safe Navigation operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 /**
- * Pipes - It's a data tranformation mechanism.
+ * Safe navigation opertor
  * 
  */
 
 import { Component } from '@angular/core';
 
-/**
- * "?X makes X as the optional property in interface"
- */
+interface Child {
+  name:string,
+  age:number
+ }
 
+ /**
+  * "children: Child[] | null" tells TS compiler that "children" property should always be there,
+  *  but it can either of type null or Child[]
+  */
 interface Passenger {
   id: number,
   fullName: string,
   checkedIn: boolean,
-  checkedInDate? : number
+  checkedInDate? : number,
+  children: Child[] | null
 }
 
 @Component({
@@ -28,12 +34,32 @@ interface Passenger {
        [class.checked-in]="pax.checkedIn">
       </span>
       {{ i }} : {{ pax.fullName }}
-      <!-- JSON pipe is quite useful for debugging -->
       <p>{{ pax | json }}</p>
-      <!-- Date pipe -->
       <div>
       Checked in date : 
       {{  pax.checkedInDate ? (pax.checkedInDate | date:'MMMM d, y' | uppercase) : "Not checked In" }}
+      </div>
+      <!-- Unsafe operation, runtime browser breaks when "pax.children" is null --> 
+      <!-- 
+      <div>
+      Children : {{ pax.children.length }}
+      </div>
+      -->
+
+      <!-- Safe op : ? -->
+      <!-- 
+      "?", safe operator, basically tells angular, if pax.children is defined, go the next steps,
+      otherwise STOP evaluating expression . So, ? is use
+      --> 
+      <div>
+      Children : {{ pax.children?.length}}
+      </div>
+      <!--
+      Also we can specify a value to be computed, when ? detects that pax.children is null/undefined,
+      it falls back to the vlaue specified after OR operator
+      -->
+      <div>
+      Children : {{ pax.children?.length || 0}}
       </div>
     </li>
   </ul>
@@ -46,16 +72,19 @@ export class AppComponent {
     id: 1,
     fullName: 'Varun Singhal',
     checkedIn: true,
-    checkedInDate : 1587400608880
+    checkedInDate : 1587400608880,
+    children : [{name: 'chota varun', age: 12}, {name: 'aur chota varun', age:9}]
   }, {
     id: 2,
     fullName: 'reeta',
-    checkedIn: false
+    checkedIn: false,
+    children: null
   }, {
     id: 3,
     fullName: 'Tina',
     checkedIn: true,
-    checkedInDate: 1587400608880
+    checkedInDate: 1587400608880,
+    children : [{name: 'choti Tina', age: 11}, {name: 'aur choti Tina', age:6}]
   }]
 
 }
