be563b24ed0dd14ea3124656aa43125d0d62ade0
varunsinghal65 <varunsinghal65@live.com>
2020-04-20 20:19:36 +0530

    Rendering Flows : ngIf, * syntax and <ng-template>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,62 @@
 /**
- *Template #ref :
+ * ngIf, * syntax and <ng-template>
  * 
- * Ng allows devs to access a HTML DOM object anywhere in the template via
- * #ref
+ * Directives allow user to instruct angular to manipulate the DOM, add more functionality,
+ * customize appearence etc.
  * 
- * Syntax
+ * There are 3 types of directives.
  * 
- * <htmlElem #reference></htmlElem>
- * <button (click)="handleClick(reference)"></button>
+ * 1. Components—directives with a template.
+ * 2. Structural directives—change the DOM layout by adding and removing DOM elements.
+ * 3. Attribute directives—change the appearance or behavior of an element, component, or another directive.
+ * 
+ * ngIf is a structural directive.
+ * 
  */
 
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector : 'app-root',
-  styleUrls : ['app.component.scss'],
-  template : `
-    <button (click)="handleClick(username.value)">Get Value</button>
-    <input [value]="name" #username/>
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <input 
+    [value]="name"
+    (input)="handleInputEvent($event.target.value)"/>
+      
+    <div *ngIf="name.length > 2">
+    Searching for.. {{ name }}
+    </div>
+
+    <!-- 
+      - The asterisk is a syntactic sugar,
+      - Under the hood, angular will wrap the above div in a <ng-template> markup element, 
+      and property bind expression to element's ngIf.
+      - <ng-template> only work in the presence of structural directives.
+      - Angular wraps the host element (to which the directive is applied) inside <ng-template>
+      and consumes the <ng-template> in the finished DOM by replacing it with diagnostic comments.
+      - Thus ng-template will never be part of final DOM.
+      - Below is what angular will actually create.
+    -->
+    <ng-template [ngIf]="name.length > 2">
+      <div>
+      Searching for.. {{ name }}
+      </div>
+    </ng-template>
+
+    
+
+
     `
 })
 
 export class AppComponent {
   title: string;
-  name: string = "Todd Motto";
-  handleClick(value:string) {
-    console.log(value);
+  name: string = '';
+  handleInputEvent(value: string) {
+    this.name = value;
   }
-  constructor () {
+  constructor() {
     this.title = 'Ultimate Angular';
   }
 
