c51c81534af55752baf889b767a15a0a978c092c
varunsinghal65 <varunsinghal65@live.com>
2020-04-21 21:20:04 +0530

    Services, Http and Observables: Data Services and Dependency Injection

--- src/app/passenger-dashboard/containers/passenger-dashoard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashoard.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Passenger} from '../../passenger-dashboard/models/passenger.interface';
+import { PassengerDashboardService } from '../passenger-dashboard.service';
 
 
 @Component({
@@ -7,14 +8,6 @@ import {Passenger} from '../../passenger-dashboard/models/passenger.interface';
     styleUrls : ['passenger-dashboard.component.scss'],
     template : `
     <passenger-count [items]="passengers"></passenger-count>
-    <!--
-    Issue here : because of the nature of JS,
-    the passenger-dashboard will not send a clone of the passengers in
-    [detail], instead it operates by passing reference,
-    Since reference is passed, when local component (passenger-detail) 's state changes
-    parent conatiner state is also impacted, this is actually not intended, since we would like to perform
-    the update only by emitting events from child
-    -->
     <div *ngFor="let pax of passengers">
     {{ pax.fullname }}
     </div>
@@ -28,48 +21,29 @@ import {Passenger} from '../../passenger-dashboard/models/passenger.interface';
 })
 export class PassengerDashboardComponent implements OnInit{
   passengers: Passenger[] = [];
+
+  /**
+   * 
+   * Constructor based DI
+   *
+   * Note, passengerDashboardSvc is declared private,
+   * so that its not exposed to the template of this class
+   * (At leats angular AOT/ or compilation should complain if you try to access)
+   * 
+   * @param paxSevice 
+   */
+  constructor(private readonly paxSevice: PassengerDashboardService) {
+  }
+
+  /**
+   * Use this hook for getting the data through services
+   */
   ngOnInit() {
     console.log("ngOnInit called");
-    this.passengers = [{
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-      checkInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-      checkInDate: null,
-      children: [{ name: 'Ted', age: 12 },{ name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-      checkInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-      checkInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-      checkInDate: null,
-      children: null
-    }];
+    this.passengers = this.paxSevice.getPassengers();
   }
 
   handleRemove(event: Passenger) {
-    /**
-     * This is a immutable state change
-     *
-     * We do not want to mutate the existing passenger, thus we use filter method,
-     * which will create return a new array
-     */
     this.passengers = this.passengers.filter((pax:Passenger)=>{
       return pax.id !== event.id;
     });
@@ -78,11 +52,6 @@ export class PassengerDashboardComponent implements OnInit{
   handleEdit(event: Passenger) {
     this.passengers = this.passengers.map((pax:Passenger)=>{
       if (pax.id === event.id) {
-        /**
-         * Immutable state change, as we are creating a new object rather than modifying the old one.
-         * Object.assign does exactly that, copying data from existing object ("pax") and then copying data from
-         * "event", the operation basically merged pax and event and creates a new instance of Passenger with the merged data.
-         */
         pax = Object.assign({}, pax, event);
       }
       return pax;
