6b4b613f0bc039e649048dd4d02d9f9bc27cc50d
varunsinghal65 <varunsinghal65@live.com>
2020-04-26 16:00:35 +0530

    Component Routing: Feature-module routes with forChild()

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -17,8 +17,21 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 import { PassengerDashboardService } from './passenger-dashboard.service';
 import { importType } from '@angular/compiler/src/output/output_ast';
 
+/**
+ * 1. Now we want to achieve something like, when user visits /passengers/1, he sees the data of pax with ID 1.
+ * 2. For that we will create child routes. So parent's path is "passengers" and child routes path's will be
+ * relative to it.
+ * 3. The first child route has a path of "", meaning its actual path is 'passengers'
+ * 4. The second child route has a path of ":id", meaning its actual path is 'passengers/id'
+ * 5. ':id', will allows us to capture the dynamic data (1) in the URL ('passengers/1')
+ * 6. Such a child route, is called dynamic route, because ':id' is dynamic.
+ */
 export const ROUTES: Routes = [{
-    path: 'passengers', component: PassengerDashboardComponent
+    path: 'passengers',
+    children: [
+        {path:'', component: PassengerDashboardComponent},
+        {path:':id', component:PassengerViewerComponent}
+    ]
 }];
 
 @NgModule({
@@ -30,7 +43,7 @@ export const ROUTES: Routes = [{
         PassengerFormComponent
     ],
     imports: [CommonModule, HttpClientModule, FormsModule, RouterModule.forChild(ROUTES)],
-    exports: [PassengerViewerComponent],
+    exports: [],
     providers: [PassengerDashboardService]
 })
 export class PassengerDashboardModule {
