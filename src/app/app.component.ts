e36b96d24bef570614395701d39a6d64ab30e505
varunsinghal65 <varunsinghal65@live.com>
2020-04-26 14:17:47 +0530

    Component Routing: RouterLink: Styling active routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,25 @@ import {Component} from '@angular/core';
   selector : 'app-root',
   styleUrls : ['app.component.scss'],
   template : `
-  <!-- This is the place where angular router will render the routed components -->
+  <nav class='nav'>
+  <!-- 
+  1. routerLinkActive is a ng directive, that will add the CSS class specified to the elem, 
+  when the elem's routerLink is activated.
+  2. Now, by default angular will apply the CSS class even if part of the routerLink URL is active.
+  meaning, for routerLink='/users/bob', css class will be applied to the elem if '/users' OR '/users/bob' is activated.
+  Similarly, in the below example, when we click on 404 link, home link is still styled with css class.
+  This is so because, ng will apply CSS class to home elem for every URL, since it's routerlink is '/', and '/' will be part of every URL.
+  To fix this, we tell angular to do an exact match using routerLinkActiveOptions.
+  3. Also, [X]='Y', ng interprets Y as a non string, X='Y', ng interprets Y as a string. 
+  -->
+    <a 
+     routerLink='/' 
+     routerLinkActive='active'
+     [routerLinkActiveOptions]='{exact: true}'>Home</a>
+    <a 
+     routerLink='/oops' 
+     routerLinkActive='active'>404</a>
+  </nav>
   <router-outlet></router-outlet>
   `
 })
