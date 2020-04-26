322ba368919276c0cea91812e2d4fdbfac1fcd8b
varunsinghal65 <varunsinghal65@live.com>
2020-04-26 15:32:27 +0530

    Component Routing: Dynamic navigation with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,32 +12,32 @@
 
 import {Component} from '@angular/core';
 
+interface NavItem {
+  name: string,
+  link: string,
+  exactLinkMatch: boolean
+}
+
 @Component({
   selector : 'app-root',
   styleUrls : ['app.component.scss'],
   template : `
   <nav class='nav'>
-  <!-- 
-  1. routerLinkActive is a ng directive, that will add the CSS class specified to the elem, 
-  when the elem's routerLink is activated.
-  2. Now, by default angular will apply the CSS class even if part of the routerLink URL is active.
-  meaning, for routerLink='/users/bob', css class will be applied to the elem if '/users' OR '/users/bob' is activated.
-  Similarly, in the below example, when we click on 404 link, home link is still styled with css class.
-  This is so because, ng will apply CSS class to home elem for every URL, since it's routerlink is '/', and '/' will be part of every URL.
-  To fix this, we tell angular to do an exact match using routerLinkActiveOptions.
-  3. Also, [X]='Y', ng interprets Y as a non string, X='Y', ng interprets Y as a string. 
-  -->
-    <a 
-     routerLink='/' 
+    <a *ngFor="let navItem of navItems"
+     [routerLink]='navItem.link' 
      routerLinkActive='active'
-     [routerLinkActiveOptions]='{exact: true}'>Home</a>
-    <a 
-     routerLink='/oops' 
-     routerLinkActive='active'>404</a>
+     [routerLinkActiveOptions]='{exact: navItem.exactLinkMatch}'>{{ navItem.name }}</a>
   </nav>
   <router-outlet></router-outlet>
   `
 })
 
 export class AppComponent {
+
+  readonly navItems: NavItem[] = [
+    { name: 'Home', link: '/', exactLinkMatch: true },
+    { name: '404', link: '/oops', exactLinkMatch: false }
+  ];
+
+
 }
